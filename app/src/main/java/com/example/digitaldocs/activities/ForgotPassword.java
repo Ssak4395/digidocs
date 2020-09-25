2e4f92a5bd45f43ea4373ce7b0c0a25d32a2de19
Jessica Yao <jyao7254@uni.sydney.edu.au>
2020-09-25 15:27:44 +0000

    ForgotPassword.java created

---
+++ app/src/main/java/com/example/digitaldocs/activities/ForgotPassword.java
@@ -0,0 +1,16 @@
+
+package com.example.digitaldocs.activities;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.digitaldocs.R;
+
+public class ForgotPassword extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.forgotpassword);
+        setContentView(R.layout.forgotpassword);
+
+    }
+}
+
