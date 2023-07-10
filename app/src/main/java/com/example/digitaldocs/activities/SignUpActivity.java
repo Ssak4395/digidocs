e524c2fab20274c40b025d53ba720a1a2f10a9b8
deepmathematic <138787626+deepmathematic@users.noreply.github.com>
2023-07-10 22:59:22 +1000

    Update SignUpActivity.java
    Better Format

--- app/src/main/java/com/example/digitaldocs/activities/SignUpActivity.java
+++ app/src/main/java/com/example/digitaldocs/activities/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.example.digitaldocs.activities;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 import com.example.digitaldocs.R;
 import com.example.digitaldocs.utilities.SignUpHandler;
